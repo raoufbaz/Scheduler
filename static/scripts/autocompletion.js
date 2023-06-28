94e167d673b0c2badd09ab93935c0f413734ef9b
Raouf Baaziz <45836036+raoufbaz@users.noreply.github.com>
2023-06-28 18:26:07 -0400

    add: upgraded page UI

--- static/scripts/autocompletion.js
+++ static/scripts/autocompletion.js
@@ -63,9 +63,3 @@ window.addEventListener("click", function(event) {
   }
 });
 
-// reset program_id value when program_title is reset.
-programInput.addEventListener("input", function(event) {
-  if (programInput.value === '') {
-    programId.value ="";
-  }
-});
---
+++ static/scripts/utils_index.js
@@ -0,0 +1,8 @@
+// reset program_id value when program_title is reset.
+programInput.addEventListener("input", function(event) {
+    if (programInput.value === '') {
+      programId.value ="";
+    }
+  });
+
+  
